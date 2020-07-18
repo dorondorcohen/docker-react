5619fa3bd58eb42491a14efc24466ba731fe08fc
unknown <doron@smartclosing.co>
2020-07-18 13:45:27 +0300

    test commit for check if travis work correctly

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          dddd
+          Hi!
         </p>
         <a
           className="App-link"
